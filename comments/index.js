acd621edcea6a99dd1aa1bcef94433314959cd0e
Casey Rowley <caseyrowley@caseys-mbp.lan>
2020-10-31 21:28:53 -0600

    comments service functioning.

--- comments/index.js
+++ comments/index.js
@@ -9,12 +9,20 @@ const commentsByPostId = {};
 
 
 app.get('/posts/:id/comments', (req, res) => {
-    
+    res.send(commentsByPostId[req.params.id] || []);
 });
 
 app.post('/posts/:id/comments', (req, res) => {
-    const id = randomBytes(4).toString('hex');
-    const { title } = req.body;
+    const commentId = randomBytes(4).toString('hex');
+    const { content } = req.body;
+
+    const comments = commentsByPostId[req.params.id] || [];
+
+    comments.push({ id: commentId, content });
+    commentsByPostId[req.params.id] = comments;
+
+    res.status(201).send(comments);
+
 
 });
 
